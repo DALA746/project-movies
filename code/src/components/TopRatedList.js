ed0c38063a40c2c65d70c5641d801c534eacaf2f
DALA746 <darya.lapata@gmail.com>
2022-11-18 21:30:02 +0100

    updated another component

--- code/src/components/TopRatedList.js
+++ code/src/components/TopRatedList.js
@@ -1,47 +1,48 @@
-import React, { useState, useEffect}  from 'react'
-import { Link }  from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import './movieList.css'
-import { TOP_RATED_URL } from '../utils/urls'
-import { Loading } from '../components/Loading'
+import './movieList.css';
+import { TOP_RATED_URL } from '../utils/urls';
+import { Loading } from '../components/Loading';
 
 export const TopRatedList = () => {
+  const [topRatedList, setTopRatedList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	const [topRatedList, setTopRatedList] = useState([])
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch(TOP_RATED_URL)
-		  .then((res) => res.json())
-		  .then((json) => {
-			setLoading(false)
-			setTopRatedList(json.results)
-		  })
-	  }, []);
+  useEffect(() => {
+    fetch(TOP_RATED_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        setLoading(false);
+        setTopRatedList(json.results);
+      });
+  }, []);
 
   return (
-			<>
-				{loading && <Loading />} 
-				<section className="movie-container"> 
-					{topRatedList.map(movie => (
-					<Link to={`/movies/${movie.id}`} key={movie.id} exact="true"> 
-						<div  className="movie-card" >
-							<div className="image-container">
-								<img 
-									src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} 
-									alt={movie.title}
-									className="image" />
-								<div className="overlay">
-									<div className="text">
-										<h1>{movie.title}</h1>
-										<p>Release Date: {movie.release_date}</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</Link>
-					))}
-				</section>
-			</>
-    );
-  }
+    <>
+      {loading && <Loading />}
+      <section className='movie-container'>
+        {topRatedList.map((movie) => (
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <div className='movie-card'>
+              <div className='image-container'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
+                  alt={movie.title}
+                  className='image'
+                />
+                <div className='overlay'>
+                  <div className='text'>
+                    <h1>{movie.title}</h1>
+                    <p>Release Date: {movie.release_date}</p>
+                    <p>{movie.overview}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </section>
+    </>
+  );
+};
